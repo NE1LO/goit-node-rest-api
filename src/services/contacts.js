57f2d8ec488ebf18b18f635711b0e11a06620cf7
Anton Smeiko <asmeiko13@gmail.com>
2024-07-09 15:31:08 +0200

    update
    hw4-validation

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,27 +1,59 @@
 import Contact from "../models/contacts.js";
+import { calculatePaginationData } from "../utils/calculatePaginationData.js";
+import { SORT_ORDER } from "../constants/index.js";
+import { parseIsFavourite } from "../utils/parseFilterParams.js";
 
-export const getAllContacts = async () => {
-  const contacts = await Contact.find();
-  return contacts;
+//#=================================================> GET ALL CONTACTS
+export const getAllContacts = async (
+  page = 1,
+  perPage = 10,
+  sortBy = "name",
+  sortOrder = SORT_ORDER.ASC,
+  filter = {}
+) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+  let contactsQuery = Contact.find()
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder });
+
+  if (filter.isFavourite !== undefined) {
+    const parsedIsFavourite = parseIsFavourite(filter.isFavourite);
+    if (parsedIsFavourite !== undefined) {
+      contactsQuery = contactsQuery
+        .where("isFavourite")
+        .equals(parseIsFavourite);
+    }
+  }
+  const contactsCount = await Contact.countDocuments();
+  const contacts = await contactsQuery.exec();
+  const paginationData = calculatePaginationData(contactsCount, limit, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
+
+//#=================================================> GET CONTACT BY ID
 export const getContactById = async (contactId) => {
-  const contact = await Contact.findById(contactId);
-  return contact;
+  return await Contact.findById(contactId);
 };
 
+//#=================================================> CREATE NEW CONTACT
 export const createContact = async (payload) => {
-  const contact = await Contact.create(payload);
-  return contact;
+  return await Contact.create(payload);
 };
 
+//#=================================================> DELETE CONTACT
 export const deleteContact = async (contactId) => {
-  const contact = await Contact.findOneAndDelete({
+  return await Contact.findOneAndDelete({
     _id: contactId,
   });
-
-  return contact;
 };
 
+//#=================================================> UPDATE CONTACT BY ID
 export const updateContact = async (contactId, payload, options = {}) => {
   const rawResult = await Contact.findOneAndUpdate(
     { _id: contactId },
